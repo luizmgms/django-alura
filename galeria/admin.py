ed5c693f26f6f5298e4bd319015234275b388800
Luiz Magno Meneses de Sousa <1143266@ifpi.int>
2023-10-05 09:14:51 -0300

    finaliza aula2.3

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "categoria", "legenda", "descricao")
+    list_display_links = ("id", "nome", "legenda")
+    search_fields = ("nome", "descricao")
+
+admin.site.register(Fotografia, ListandoFotografias)
