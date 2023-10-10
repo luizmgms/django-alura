f3c5b672c06418b6f471faa2d8dc1f0e7bf8d82f
Luiz Magno Meneses de Sousa <1143266@ifpi.int>
2023-10-10 16:09:09 -0300

    finalizado aula 3.3 curso 2

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from usuarios.forms import LoginForms, CadastroForms
+
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -9,4 +11,27 @@ def login(request):
 
 def cadastro(request):
     form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            if form["senha1"].value() != form["senha2"].value():
+                return redirect ('cadastro')
+
+            nome=form['nome_cadastro'].value()
+            email=form['email'].value()
+            senha=form['senha1'].value()
+
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            return redirect('login')
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
